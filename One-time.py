6dfd7ecf8eb3a9b66f96fe6bc99af9f523f23e2f
Hxrshrathore <rathore_harsh@outlook.in>
2023-11-09 14:03:44 +0530

    Changes in Data sample creation

--- One-time.py
+++ One-time.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Create sample data
 X = np.linspace(0, 10, 100)  # Input data
-y = 2 * X + 1 + np.random.normal(0, 1, 100)  # Output data with some noise
+y = 2 * X + 1 + 69 + np.random.normal(0, 1, 100)  # Output data with some noise
 
 # Create a DataFrame
 data = pd.DataFrame({'X': X, 'y': y})
